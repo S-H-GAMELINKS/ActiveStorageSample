7cf2ebc95e36291a3377d1565f8cbe08f981bcaa
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-09 12:25:23 +0900

    using many images

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
     before_action :set_comment, only: [:show, :edit, :update]
-    
+
     def new
         @comment = Comment.new
     end
@@ -27,6 +27,6 @@ class CommentsController < ApplicationController
         end
 
         def comment_params
-            params.require(:comment).permit(:content, :image)
+            params.require(:comment).permit(:content, :images)
         end
 end
