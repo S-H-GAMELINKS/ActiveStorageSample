71061252e3bdb7333a813704508629c090dc674b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-09 12:20:50 +0900

    add comments_controller for methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,32 @@
 class CommentsController < ApplicationController
+    before_action :set_comment, only: [:show, :edit, :update]
+    
+    def new
+        @comment = Comment.new
+    end
+    
+    def create
+        @comment = Comment.create comment_params
+        redirect_to @comment
+    end
+    
+    def show
+    end
+    
+    def edit
+    end
+    
+    def update
+        @comment.update comment_params
+        redirect_to @comment
+    end
+
+    private
+        def set_comment
+            @comment = Comment.find(params[:id])
+        end
+
+        def comment_params
+            params.require(:comment).permit(:content, :image)
+        end
 end
